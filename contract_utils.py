853d37bc092fcf227bb536e59fc469ae893e9e46
Jakob Steimle <jakob.steimle@tum.de>
2022-11-19 22:43:04 +0100

    Temporary approval contract

--- contract_utils.py
+++ contract_utils.py
@@ -6,17 +6,95 @@ from time import time, sleep
 
 
 def approval_contract():
-    on_create = Approve()
-    on_txn = Seq(
-        If(Gtxn[Txn.group_index()-Int(1)].amount()
-           >= Int(1000000)).Then(
-            Approve()
-        ).Else(
-            Reject()
-        ))
+    credit_giver_key = Bytes("credit_giver_key")
+    credit_taker_key = Bytes("credit_taker_key")
+    security_giver_key = Bytes("security_giver")
+    security_giver_value = Bytes("security_giver_value")
+    escrow_value = Bytes("escrow_value")
+    security_giver_goal_sum = Bytes("security_giver_goal_sum")
+    number_of_security_givers = Bytes("securty_giver")
+    credit = Bytes("credit")
+    interest_credit = Bytes("interest_credit")
+    interest_security = Bytes("interest_security")
+    security_start_time = Bytes("security_start")
+    security_end_time = Bytes("securty_end")
+    credit_start_time = Bytes("credit_start")
+    credit_end_time = Bytes("credit_start")
+    state = Bytes("state")
+
+    # @Subroutine(TealType.none)
+    # def transact(receiver: Expr, amount: Expr) -> Expr:
+    #     return Seq(
+    #         InnerTxnBuilder.Begin(),
+    #         InnerTxnBuilder.SetFields(
+    #             {
+    #                 TxnField.type_enum: TxnType.Payment,
+    #                 TxnField.amount: amount,
+    #                 TxnField.receiver: receiver,
+    #             }
+    #         ),
+    #         InnerTxnBuilder.Submit(),
+    #     )
+
+    on_create = Seq(
+        App.globalPut(credit_giver_key, Bytes("VELO7H2K7D72I2TIKZFPVMQH57PUD4R5DXXBA4Y4SMHBZQS42NJHNQS2YI")),
+        App.globalPut(credit_taker_key,
+                      Bytes(b'\x1f\x83\x1d9\xda\x9dZH5Qt\xfb]L\t\x12\xe8{3\xb0\xb2\xaa\xe9^(\x88|{\xea!\xa7\xcb')),
+        App.globalPut(security_start_time, Bytes("")),
+        App.globalPut(security_giver_value, Int(0)),
+        App.globalPut(escrow_value, Int(0)),
+        App.globalPut(security_giver_goal_sum, Int(100000)),
+        App.globalPut(credit, Int(500000)),
+        App.globalPut(interest_credit, Int(0)),
+        App.globalPut(interest_security, Int(0)),
+        App.globalPut(security_end_time, Bytes("")),
+        App.globalPut(credit_start_time, Bytes("")),
+        App.globalPut(credit_end_time, Bytes("")),
+        App.globalPut(state, Int(0)),
+        Approve()
+    )
+    # The contract can be closed, if the escrow contains (100%+InterestRate)of Credit and (100%+InterestRate)of Security
+    pay_credit_escrow_goal = (App.globalGet(credit) * (App.globalGet(interest_credit) + Int(100)) / Int(100)) + (
+                App.globalGet(security_giver_goal_sum) * (App.globalGet(interest_security) + Int(100)) / Int(100))
+    on_setup = Seq(
+        App.globalPut(escrow_value, Add(App.globalGet(escrow_value), Gtxn[Txn.group_index() - Int(1)].amount())),
+        If(App.globalGet(escrow_value) >= App.globalGet(credit)).Then(
+            App.globalPut(state, Int(1))
+        ),
+        Approve()
+    )
+    on_security = Seq(
+        # Todo: Stop if too late for security
+        App.globalPut(escrow_value, Add(App.globalGet(escrow_value), Gtxn[Txn.group_index() - Int(1)].amount())),
+        # Transmit credit if enough security
+        If(App.globalGet(escrow_value) >= App.globalGet(security_giver_goal_sum)).Then(
+            Seq(
+                App.globalPut(state, Int(2)),
+                InnerTxnBuilder.Begin(),
+                InnerTxnBuilder.SetFields(
+                    {
+                        TxnField.type_enum: TxnType.Payment,
+                        TxnField.amount: App.globalGet(credit),
+                        TxnField.receiver: App.globalGet(credit_taker_key),
+                    }
+                ),
+                InnerTxnBuilder.Submit(),
+                App.globalPut(escrow_value, Minus(App.globalGet(escrow_value), App.globalGet(credit)))
+            )
+        ),
+        Approve(),
+    )
+    on_pay_credit = Seq(
+        # Todo: Stop if too late for credit
+        App.globalPut(escrow_value, Add(App.globalGet(escrow_value), Gtxn[Txn.group_index() - Int(1)].amount())),
+        Approve()
+    )
+    pay_credit_goal = Add(App.globalGet(security_giver_goal_sum), App.globalGet(credit))
     program = Cond(
         [Txn.application_id() == Int(0), on_create],
-        [Txn.on_completion() == OnComplete.NoOp, on_txn]
+        [App.globalGet(state) < Int(1), on_setup],
+        [App.globalGet(state) < Int(2), on_security],
+        [App.globalGet(state) < Int(3), on_pay_credit]
     )
     return program
 
